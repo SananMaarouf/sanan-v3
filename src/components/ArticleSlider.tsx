2057a9fb68b1ac5b77b15a6427f23d2fa6157f3c
Sanan Maarouf <sanan.adnan97@gmail.com>
2024-01-12 00:08:49 +0100

    hover effects on articleslider

--- src/components/ArticleSlider.tsx
+++ src/components/ArticleSlider.tsx
@@ -3,7 +3,10 @@ import Carousel from 'react-grid-carousel';
 import { useTranslation } from 'next-i18next';
 import { ArticleTile } from 'src/components/features/article/ArticleTile';
 import { PageBlogPostFieldsFragment } from '@src/lib/__generated/sdk';
-
+import { CtfImage } from '@src/components/features/contentful';
+import { useContentfulInspectorMode } from '@contentful/live-preview/react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 interface SliderProps extends HTMLProps<HTMLDivElement> {
   articles?: Array<PageBlogPostFieldsFragment | null>;
 }
@@ -31,9 +34,27 @@ export const ArticleSlider = ({ articles }: SliderProps) => {
       {articles.map(article => {
         return article ? (
           <Carousel.Item key={article.sys.id}>
-            <div className="flex h-full w-full flex-col">
-              <ArticleTile article={article} />
-            </div>
+            <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+              <div className="flex h-full w-full flex-col p-3">
+                <Link className="flex h-full w-full flex-col" href={`/${article.slug}`}>
+                  <div className="flex h-full w-full flex-col overflow-hidden rounded-2xl border border-gray-300 shadow-sm">
+                    {article.coverImage && (
+                      <CtfImage
+                        nextImageProps={{ className: 'object-cover aspect-[16/10] w-full' }}
+                        {...article.coverImage}
+                      />
+                    )}
+                    <div className="mb-2 mt-2 flex h-20 px-4 md:h-28">
+                      {article.title && (
+                        <h2 className="text-gray800 text-lg md:mb-3 md:text-sm lg:text-2xl landscape:text-xl">
+                          {article.title}
+                        </h2>
+                      )}
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </motion.div>
           </Carousel.Item>
         ) : null;
       })}
